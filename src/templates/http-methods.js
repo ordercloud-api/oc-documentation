7eea557f2a577aa87219015837927f5cf1e627b7
Alexa Rarick <ararick@four51.com>
2019-06-21 10:02:58 -0500

    Add layout component to http methods template

--- src/templates/http-methods.js
+++ src/templates/http-methods.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import Layout from "../components/layout"
 
 // import '../css/blog-post.css'; // make it pretty!
 
@@ -8,16 +9,18 @@ export default function Template({
 }) {
   const { markdownRemark: post } = data // data.markdownRemark holds our post data
   return (
-    <div className="blog-post-container">
-      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
-      <div className="blog-post">
-        <h1>{post.frontmatter.title}</h1>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
+    <Layout>
+      <div className="blog-post-container">
+        <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
+        <div className="blog-post">
+          <h1>{post.frontmatter.title}</h1>
+          <div
+            className="blog-post-content"
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
 
