f368bd29f68337bbcbdf52adea8f599ae9eea031
Alexa Rarick <ararick@four51.com>
2019-06-21 11:01:51 -0500

    add material ui plugin

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ const ListLink = props => (
 )
 
 export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ margin: `3rem auto`, maxWidth: `60%`, padding: `0 1rem` }}>
     <header style={{ marginBottom: `1.5rem` }}>
       <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
         <h3 style={{ display: `inline` }}>MySweetSite</h3>
