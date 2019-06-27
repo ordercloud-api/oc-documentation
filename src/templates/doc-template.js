50d579d8d19c45427648760e3283f80db3ba1144
Alexa Rarick <ararick@four51.com>
2019-06-27 15:04:52 -0500

    some more structure to the app, add react-router-dom

--- src/templates/doc-template.js
+++ src/templates/doc-template.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import Layout from "../components/layout"
+import Layout from "../components/Layout/Layout"
 
 // import '../css/blog-post.css'; // make it pretty!
 
