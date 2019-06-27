4156339ed4f6638fc2edb1c5b858257cafb3b931
Crhistian Ramirez <cramirez@four51.com>
2019-06-27 17:03:17 -0500

    clean up comments in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `A Blog`,
-    description: `A blog tutorial for gatsby with remarkable plugin`,
+    title: `OrderCloud Documentation`,
+    description: `Documentation for OrderCloud's B2B eCommerce API`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -9,9 +9,7 @@ module.exports = {
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // You can have multiple instances of this plugin
-    // to read source nodes from different locations on your
-    // filesystem.
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -35,25 +33,9 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/oc-favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/oc-favicon.png`,
       },
     },
-    {
-      resolve: `gatsby-plugin-material-ui`,
-      // If you want to use styled components, in conjunction to Material-UI, you should:
-      // - Change the injection order
-      // - Add the plugin
-      options: {
-        // stylesProvider: {
-        //   injectFirst: true,
-        // },
-      },
-      // 'gatsby-plugin-styled-components',
-    },
-    
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
     {
       resolve: "gatsby-transformer-remark",
       options: {
