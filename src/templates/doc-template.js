6c3827c516b9bc7b7058709b00b056222ef26b86
Alexa Rarick <ararick@four51.com>
2019-06-28 10:09:55 -0500

    add directional buttons

--- src/templates/doc-template.js
+++ src/templates/doc-template.js
@@ -3,6 +3,8 @@ import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout/Layout';
 import RightMenu from '../components/Layout/RightMenu';
 import '../styles/doc-template.css';
+import { Button } from '@material-ui/core';
+import { Link } from 'gatsby';
 
 const tableOfContents = require('../pages/table-of-contents.json');
 
@@ -11,6 +13,27 @@ export default function Template({
 }) {
   const { markdownRemark: post } = data
   const gitHubUrl = 'https://github.com/ordercloud-api/oc-documentation/tree/development/src/pages/docs';
+  const section = tableOfContents.sections.find((section) => post.frontmatter.path.indexOf(section.path) > -1);
+  const sectionIndex = tableOfContents.sections.findIndex((section) => post.frontmatter.path.indexOf(section.path) > -1);
+  const guideIndex = section.guides.findIndex((guide) => `${section.path}${guide}` === post.frontmatter.path);
+
+  function directionalButton(direction) {
+    const newGuideIndex = direction === 'Previous' ? guideIndex - 1 : guideIndex + 1;
+    const newSectionIndex = direction === 'Previous' ? sectionIndex - 1 : sectionIndex + 1;
+    const newSection = tableOfContents.sections[newSectionIndex];
+    return (
+      guideIndex > 0 ? (
+        <Button>
+          <Link to={`${section.path}${section.guides[newGuideIndex]}`}>{direction} Guide</Link>
+        </Button>
+      ) : sectionIndex > 0 ? (
+        <Button>
+          <Link to={`${newSection.path}${newSection.guides[0]}`}>{direction} Section</Link>
+        </Button> 
+      ) : null
+    )
+  }
+
   return (
     <Layout>
       <div className="documentation-container" style={{margin: '0 3rem', display: 'flex', flexDirection: 'row'}}>
@@ -21,6 +44,8 @@ export default function Template({
             className="documentation-contents"
             dangerouslySetInnerHTML={{ __html: post.html }}
           />
+          {directionalButton('Previous')}
+          {directionalButton('Next')}         
           <a href={`${gitHubUrl}${post.frontmatter.path}.md`} target="_blank">Contribute to this doc</a>
         </div>
         <RightMenu style={{ maxWidth: '30%'}} tableOfContents={tableOfContents.sections} />
