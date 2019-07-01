c14cd3b7ba22f76e4d96d159b21d7fb6881fe66f
Alexa Rarick <ararick@four51.com>
2019-07-01 13:04:32 -0500

    remove excess logic for next/previous guide/section buttons

--- src/components/Layout/RightMenu.js
+++ src/components/Layout/RightMenu.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { ListLink } from '../Shared/ListLink';
 import { groupBy as _groupBy, forEach as _forEach } from 'lodash';
 
-export default function RightMenu({ tableOfContents}) {
-  const sectionsWithGuides = _groupBy(tableOfContents, 'node.frontmatter.section');
+export default function RightMenu({ tableOfContents }) {
   let contentsArray = [];
-  _forEach(sectionsWithGuides, (section, title) => contentsArray = [...contentsArray, {title: title, sections: section.map((s) => s.node)}]);
+  _forEach(tableOfContents, (section, title) => contentsArray = [...contentsArray, {title: title, sections: section.map((s) => s.node)}]);
   return (
     <div>
-      { contentsArray.map((guideSection) => {
+      { contentsArray.map((guideSection, index) => {
           return (
-            <div>
+            <div key={index}>
               <h2>{guideSection.title}</h2>
               <ul>
                 { guideSection.sections.map((s) => <ListLink key={s.id} guideProps={{ path: `${s.frontmatter.path}`, title: s.frontmatter.title}} />)}
