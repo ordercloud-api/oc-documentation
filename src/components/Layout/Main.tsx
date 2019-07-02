cd998d34532f28fc1500a09e0e25972d49b23724
Alexa Rarick <ararick@four51.com>
2019-07-02 08:46:30 -0500

    correct the use of styles in Main component

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -22,45 +22,48 @@ const styles = (theme: Theme) =>
     },
   });
 
-class Main extends React.Component<any> {
-  public render() {
-    const { tableOfContents } = this.props;
-    const sectionsWithGuides = _groupBy(tableOfContents.allMarkdownRemark.edges, 'node.frontmatter.section');
-    let contentsArray = [];
-    _forEach(sectionsWithGuides, (section, title) => contentsArray = [...contentsArray, {title: title, sections: section.map((s) => s.node)}]);
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              Welcome to OrderCloud
-              <Button>Intro to OrderCloud</Button>
-              <Button>Quick Start Guide</Button>
-            </Paper>
-          </Grid>
-          { contentsArray.map((section, index) => {
-            return (
-              <Grid item xs={12} sm={6} key={index}>
+  const Main = withStyles(styles)(
+    class extends React.Component<any> {
+      public render() {
+        const { tableOfContents, classes } = this.props;
+        const sectionsWithGuides = _groupBy(tableOfContents.allMarkdownRemark.edges, 'node.frontmatter.section');
+        let contentsArray = [];
+        _forEach(sectionsWithGuides, (section, title) => contentsArray = [...contentsArray, {title: title, sections: section.map((s) => s.node)}]);
+        return (
+          <div className={classes.root}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
                 <Paper className={classes.paper}>
-                  <h2>{section.title}</h2>
-                  <ul>
-                    { section.sections.map((s) => {
-                      return (
-                        <ListLink key={s.id} guideProps={{ path: s.frontmatter.path, title: s.frontmatter.title}} />
-                      )
-                    }) }
-                  </ul>
+                  Welcome to OrderCloud
+                  <Button>Intro to OrderCloud</Button>
+                  <Button>Quick Start Guide</Button>
                 </Paper>
               </Grid>
-            )
-          }) }
-        </Grid>
-      </div>
-    )
-  }
-}
+              { contentsArray.map((section, index) => {
+                return (
+                  <Grid item xs={12} sm={6} key={index}>
+                    <Paper className={classes.paper}>
+                      <h2>{section.title}</h2>
+                      <ul>
+                        { section.sections.map((s) => {
+                          return (
+                            <ListLink key={s.id} guideProps={{ path: s.frontmatter.path, title: s.frontmatter.title}} />
+                          )
+                        }) }
+                      </ul>
+                    </Paper>
+                  </Grid>
+                )
+              }) }
+            </Grid>
+          </div>
+        )
+      }
+    }
+  )
 
-export default withStyles(styles, { withTheme: true })(() => (
+
+export default (() => (
   <StaticQuery query={graphql`query {
     allMarkdownRemark(
       sort: { order: ASC, fields: [frontmatter___section] }
