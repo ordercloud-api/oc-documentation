01f173ec5666a941f5e05d87730682a27938f3a6
Eric <esitarz@four51.com>
2019-07-11 16:47:27 -0500

    change app name?

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `ordercloud documenation`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
