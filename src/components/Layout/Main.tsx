4220c3816eca320f96512f66566bd98126500838
Eric <esitarz@four51.com>
2019-07-15 15:19:53 -0500

    Main List Card Styling

--- src/components/Layout/Main.tsx
+++ src/components/Layout/Main.tsx
@@ -7,7 +7,7 @@ import ListLink from '../Shared/ListLink'
 import Jumbotron from '../Shared/Jumbotron'
 import { StaticQuery, graphql } from 'gatsby'
 import utility from '../Shared/utility'
-import { Typography, Container } from '@material-ui/core'
+import { Typography, Container, List } from '@material-ui/core'
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -15,21 +15,32 @@ const styles = (theme: Theme) =>
       flexGrow: 1,
       overflow: 'hidden',
     },
-    cardGroup: {
-      marginTop: theme.spacing(3),
-      marginBottom: theme.spacing(3),
+    cardContainer: {
+      display: 'flex',
+      flex: '1 1 auto',
+      'paperTitle:nth-of-type(1)': {
+        backgroundImage:
+          'url(https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)',
+      },
+      'paperTitle:nth-of-type(2)': {
+        backgroundImage:
+          'url(https://images.unsplash.com/photo-1559762691-617a33825bc6?ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80)',
+      },
+      'paperTitle:nth-of-type(3)': {
+        backgroundImage:
+          'url(https://images.unsplash.com/photo-1560131323-29d50e8d1b22?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2167&q=80)',
+      },
     },
     paperCard: {
       position: 'relative',
-      minHeight: '35vh',
+      minHeight: '50vh',
       display: 'flex',
       flexFlow: 'column nowrap',
       alignItems: 'center',
     },
     paperTitle: {
       width: '100%',
-      backgroundImage:
-        'url(https://images.unsplash.com/photo-1563089145-599997674d42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80)',
+      backgroundColor: theme.palette.primary.dark,
       backgroundSize: 'cover',
       minHeight: '15vh',
       display: 'flex',
@@ -38,7 +49,7 @@ const styles = (theme: Theme) =>
       justifyContent: 'center',
       color: 'white',
     },
-    paperList: {
+    paperBody: {
       position: 'absolute',
       top: '12vh',
       margin: '0 auto',
@@ -47,6 +58,10 @@ const styles = (theme: Theme) =>
       color: 'white',
       minHeight: 'min-content',
       zIndex: 1,
+    },
+    paperList: {
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr',
     },
     //utility classes
     mr3: {
@@ -79,51 +94,46 @@ const Main = withStyles(styles)(
 
       return (
         <div className={classes.root}>
-          <Grid container spacing={3}>
-            {sections.map((section, index) =>
-              section.title === 'Getting Started' ? (
-                <Grid item xs={12} sm={12} key={index}>
-                  <Jumbotron />
-                </Grid>
-              ) : (
-                <Grid
-                  className={classes.cardGroup}
-                  item
-                  xs={12}
-                  sm={6}
-                  key={index}
-                >
-                  {section.guides.filter(c => !c.frontmatter.hidden).length >
-                  0 ? (
-                    <div className={classes.paperCard}>
-                      <Paper className={classes.paperTitle}>
-                        <Typography variant="h4" component="h2">
-                          {section.title}
-                        </Typography>
-                      </Paper>
-                      <Paper className={classes.paperList}>
-                        <ul>
-                          {section.guides
-                            .filter(c => !c.frontmatter.hidden)
-                            .map(s => {
-                              return (
-                                <ListLink
-                                  key={s.id}
-                                  guideProps={{
-                                    path: s.frontmatter.path,
-                                    title: s.frontmatter.title,
-                                  }}
-                                />
-                              )
-                            })}
-                        </ul>
-                      </Paper>
-                    </div>
-                  ) : null}
-                </Grid>
-              )
-            )}
-          </Grid>
+          <Jumbotron />
+          <Container maxWidth="xl">
+            <Grid container spacing={5} className={classes.cardContainer}>
+              {sections.map((section, index) =>
+                section.title === 'Getting Started' ? (
+                  <Grid item xs={12} sm={12} key={index}></Grid>
+                ) : (
+                  <Grid item xs={12} sm={6} key={index}>
+                    {section.guides.filter(c => !c.frontmatter.hidden).length >
+                    0 ? (
+                      <div className={classes.paperCard}>
+                        <Paper className={classes.paperTitle}>
+                          <Typography variant="h4" component="h2">
+                            {section.title}
+                          </Typography>
+                        </Paper>
+                        <Paper className={classes.paperBody}>
+                          <List className={classes.paperList} component="nav">
+                            {section.guides
+                              .filter(c => !c.frontmatter.hidden)
+                              .map(s => {
+                                return (
+                                  <ListLink
+                                    key={s.id}
+                                    guideProps={{
+                                      path: s.frontmatter.path,
+                                      title: s.frontmatter.title,
+                                    }}
+                                  />
+                                )
+                              })}
+                          </List>
+                        </Paper>
+                      </div>
+                    ) : null}
+                  </Grid>
+                )
+              )}
+            </Grid>
+          </Container>
         </div>
       )
     }
