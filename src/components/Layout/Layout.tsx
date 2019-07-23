2c0591f6abe254dec5073661190d766213b8a959
Eric <esitarz@four51.com>
2019-07-23 14:32:01 -0500

    Wrap In OC Theme

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,15 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import { ThemeProvider } from '@material-ui/styles'
+import ORDERCLOUD_THEME from '../../theme/theme.constants'
 
 export default ({ children }) => (
-  <Fragment>
-    <Header siteTitle="OrderCloud Documentation" />
-    {children}
-    <Footer />
-  </Fragment>
+  <React.Fragment>
+    <ThemeProvider theme={ORDERCLOUD_THEME}>
+      <Header siteTitle="OrderCloud Documentation" />
+      {children}
+      <Footer />
+    </ThemeProvider>
+  </React.Fragment>
 )
