81f09c16eaa41a3f20cbeebe259cb206f99df49d
Eric <esitarz@four51.com>
2019-07-25 09:11:29 -0500

    Add Mobile Bottom Navigation

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,13 +3,17 @@ import Header from './Header'
 import Footer from './Footer'
 import { ThemeProvider } from '@material-ui/styles'
 import ORDERCLOUD_THEME from '../../theme/theme.constants'
+import { BottomNavigation, Hidden } from '@material-ui/core'
+import MobileNav from './MobileNav'
 
 export default ({ children }) => (
   <React.Fragment>
     <ThemeProvider theme={ORDERCLOUD_THEME}>
       <Header siteTitle="OrderCloud Documentation" />
       {children}
-      <Footer />
+      <Hidden mdUp>
+        <MobileNav />
+      </Hidden>
     </ThemeProvider>
   </React.Fragment>
 )
