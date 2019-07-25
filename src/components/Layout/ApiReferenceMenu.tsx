a2c9d3031a6ac995c036506c4eee61cb23aab74a
Alexa Rarick <ararick@four51.com>
2019-07-25 17:39:02 -0500

    utilize open api spec instead of getting data from API

--- src/components/Layout/ApiReferenceMenu.tsx
+++ src/components/Layout/ApiReferenceMenu.tsx
@@ -1,43 +1,74 @@
 import React from 'react';
+import { groupBy as _groupBy, remove as _remove } from 'lodash';
 import { Paper, Collapse, List, ListItem, ListItemText, Typography } from '@material-ui/core';
+
+interface ApiReferenceProps {
+  name: string;
+  x_section_id: string;
+  x_id: string;
+  description: string
+}
 
 export default function ApiReferenceMenu(props) {
   const { apiReference } = props;
-  console.log(apiReference);
+  // const sections: ApiReferenceProps[] = _groupBy(_remove(apiReference, (ref: ApiReferenceProps) => ref.x_section_id != null), 'x_section_id');
+  const sectionIds = apiReference.filter(ref => ref.x_id);
+  console.log('sections', sectionIds);
   return (
     <div>
-      {apiReference && apiReference.Sections ? apiReference.Sections.map((section, index) => {
+      {apiReference.map((section, index) => {
         return (
-          <div>
-            {apiReference.Resources.map(r => {
-              if (r.Section.toLowerCase() === section.replace(/\s/g, '').toLowerCase()) {
-                return (
-                  <Section key={index} section={section} resources={r} />
-                )
-              }
-            })}
-          </div>
+          <Section section={section} sectionTitle={index} />
         )
-      }) : null}
+      })}
     </div>
   )
 }
 
 function Section(props) {
-  const { section, resources } = props;
-  debugger;
+  const { section, sectionTitle } = props;
+  const [open, setOpen] = React.useState(false);
+
+  function handleClick() {
+    setOpen(!open)
+  }
+
   return (
     <List>
-      <ListItem>
+      <ListItem button onClick={handleClick}>
         <ListItemText>
           <Typography>
-            {section}
-            {resources.map(r => <p>{r}</p>)}
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              {section.x_section_id ? sectionTitle : (
+                <Resource resource={section} />
+              )}
+            </Collapse>
           </Typography>
         </ListItemText>
       </ListItem>
-      <Collapse>
-      </Collapse>
+    </List>
+  )
+}
+
+function Resource(props) {
+  const { resource } = props;
+  const [open, setOpen] = React.useState(false);
+
+  function handleClick() {
+    setOpen(!open)
+  }
+
+  return (
+    <List>
+      <ListItem button onClick={handleClick}>
+        <ListItemText>
+          <Typography>
+            <Collapse in={open} timeout="auto" unmountOnExit>
+              {resource.name}
+            </Collapse>
+          </Typography>
+        </ListItemText>
+      </ListItem>
     </List>
   )
 }
--- src/components/Templates/ApiReference.tsx
+++ src/components/Templates/ApiReference.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import apiReferenceUtil from '../Shared/apiReferenceUtility';
 import Layout from '../Layout/Layout';
 import { withStyles, Theme, createStyles, Container, Grid } from '@material-ui/core';
 import ApiReferenceMenu from '../Layout/ApiReferenceMenu';
+import { graphql, StaticQuery } from 'gatsby';
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -14,24 +14,12 @@ const styles = (theme: Theme) =>
 
 const ApiReference = withStyles(styles)(
   class extends React.Component<any> {
-
-    public state = {
-      docs: null
-    };
-
-    public async componentDidMount() {
-      apiReferenceUtil.GetAllDocs().then(docs => {
-        console.log(docs);
-        this.setState({ docs });
-      });
-    }
-
     public render() {
-      const { classes } = this.props;
+      const { classes, apiReference } = this.props;
       return (
         <Layout>
           <Container maxWidth="lg">
-            <ApiReferenceMenu apiReference={this.state.docs ? this.state.docs.data : null} />
+            <ApiReferenceMenu apiReference={apiReference} />
             {/* <Grid container className={classes.docContainer} spacing={3}>
               <Grid item xs={9}>
                 {this.state.docs ? this.state.docs.data.Sections.map(s => <p key={s}>{s}</p>) : null}
@@ -47,4 +35,24 @@ const ApiReference = withStyles(styles)(
 
 
 
-export default ApiReference
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allSrcJson {
+          edges {
+            node {
+              tags {
+                name
+                x_section_id
+                x_id
+                description
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => <ApiReference apiReference={data.allSrcJson.edges[0].node.tags} />}
+  />
+)
