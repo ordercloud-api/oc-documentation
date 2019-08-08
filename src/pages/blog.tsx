0c48563ae91f31bea59d6aa20fdc87f51a51ba93
Eric <esitarz@four51.com>
2019-08-08 17:06:44 -0500

    Blog Card Initial Styling

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -5,18 +5,88 @@ import {
   makeStyles,
   Container,
   Grid,
+  Typography,
+  Card,
+  CardMedia,
+  CardActions,
+  CardContent,
+  Avatar,
+  IconButton,
+  Icon,
+  Tooltip,
 } from '@material-ui/core/'
+import { Share } from '@material-ui/icons'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import utility from '../utility'
 import Layout from '../components/Layout/Layout'
+import placeholderImg from '../assets/images/placeholder__blog.jpg'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    card: {},
-    title: {},
-    container: {},
-    body: {},
+    cardBase: {
+      transition: '0.3s',
+      maxWidth: 304,
+      margin: 'auto',
+      boxShadow: '0 0 20px 0 rgba(0,0,0,0.12)',
+      '&:hover': {
+        transform: 'translateY(-3px)',
+        boxShadow: '0 4px 20px 0 rgba(0,0,0,0.12)',
+      },
+    },
+    cardImg: {
+      paddingTop: '56.25%',
+      position: 'relative',
+      '&::after': {
+        content: '" "',
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+        borderBottom: '32px solid #ffffff',
+        borderLeft: '400px solid transparent',
+      },
+    },
+    cardRibbon: {
+      position: 'absolute',
+      top: theme.spacing(2),
+      left: theme.spacing(2),
+      backgroundColor: theme.palette.primary.main,
+      color: '#ffffff !important',
+      padding: '2px 8px',
+      boxShadow: '0 2px 12px 2px rgba(0,0,0,0.5)',
+      borderTopLeftRadius: 2,
+      borderBottomLeftRadius: 2,
+    },
+    cardAuthor: {
+      position: 'absolute',
+      right: '12%',
+      bottom: 4,
+      transform: 'translateY(20%)',
+      width: 48,
+      height: 48,
+      zIndex: 1,
+      '&:after': {
+        content: '" "',
+        position: 'absolute',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+      },
+    },
+    cardTitle: {
+      lineHeight: '1.4',
+    },
+    MuiCardContentRoot: {
+      textAlign: 'left',
+      padding: theme.spacing(3),
+    },
+    MuiCardActionsRoot: {
+      padding: `0 ${theme.spacing(3)} ${theme.spacing(3)}`,
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
   })
 )
 
@@ -71,30 +141,52 @@ export default function BlogListComponent(props: BlogListProps) {
   return (
     <Layout>
       <Container maxWidth="lg">
+        <Helmet title={`OrderCloud Blog`} />
         <Grid container className={classes.container} spacing={3}>
-          <Helmet title={`OrderCloud Blog`} />
-          <div className={classes.body}>
-            {data.allMdx.edges.map(edge => {
-              return (
-                <div key={edge.node.id}>
-                  <h3>
-                    <Link
-                      style={{ boxShadow: `none` }}
-                      to={utility.resolvePath(edge.node.fileAbsolutePath)}
-                    >
-                      {edge.node.frontmatter.title}
+          {data.allMdx.edges.map(edge => {
+            return (
+              <Grid item xs={3}>
+                <Card className={classes.cardBase}>
+                  <CardMedia className={classes.cardImg} image={placeholderImg}>
+                    <div className={classes.cardRibbon}>
+                      <Typography variant="body2">Ribbon Boye</Typography>
+                    </div>
+                    <Avatar
+                      className={classes.cardAuthor}
+                      src={
+                        'https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80'
+                      }
+                    />
+                  </CardMedia>
+                  <CardContent className={classes.MuiCardContentRoot}>
+                    <Link to={utility.resolvePath(edge.node.fileAbsolutePath)}>
+                      <Typography className={classes.cardTitle} variant="h6">
+                        {edge.node.frontmatter.title}
+                      </Typography>
                     </Link>
-                  </h3>
-                  <small>{edge.node.frontmatter.date}</small>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: edge.node.frontmatter.summary,
-                    }}
-                  />
-                </div>
-              )
-            })}
-          </div>
+                    <Typography
+                      variant="body2"
+                      dangerouslySetInnerHTML={{
+                        __html: edge.node.frontmatter.summary,
+                      }}
+                    />
+                  </CardContent>
+                  <CardActions className={classes.MuiCardActionsRoot}>
+                    <Typography variant={'caption'}>
+                      {edge.node.frontmatter.date}
+                    </Typography>
+                    <div>
+                      <Tooltip title="Share" placement="left">
+                        <IconButton>
+                          <Share />
+                        </IconButton>
+                      </Tooltip>
+                    </div>
+                  </CardActions>
+                </Card>
+              </Grid>
+            )
+          })}
         </Grid>
       </Container>
     </Layout>
