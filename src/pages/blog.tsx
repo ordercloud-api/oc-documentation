69ae1f890d5da8ddd41df70d933f7c63cca0b6dd
Eric <esitarz@four51.com>
2019-10-29 11:15:34 -0500

    Remove unnecessary z-indexing

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -33,7 +33,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     blogContainer: {
       marginTop: -theme.spacing(14),
-      zIndex: 1,
     },
     title: {
       marginBottom: theme.spacing(1),
