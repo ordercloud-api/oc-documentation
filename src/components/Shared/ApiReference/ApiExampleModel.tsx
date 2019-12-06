92d6428ffa8372fe40c51895c3d52e3267fc5559
Alexa Snyder <asnyder@four51.com>
2019-12-06 10:37:38 -0600

    remove unused styles
    OP-85

--- src/components/Shared/ApiReference/ApiExampleModel.tsx
+++ src/components/Shared/ApiReference/ApiExampleModel.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/styles';
 import { Theme, createStyles, Typography } from '@material-ui/core';
-import { flame, sunset, maroon, sherpablue, seafoam } from '../../../theme/ocPalette.constants';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -9,21 +8,6 @@ const useStyles = makeStyles((theme: Theme) =>
             padding: theme.spacing(2),
             backgroundColor: theme.palette.common.black,
             color: theme.palette.common.white,
-        },
-        string: {
-            color: flame[600]
-        },
-        boolean: {
-            color: sunset[600]
-        },
-        array: {
-            color: maroon[600]
-        },
-        object: {
-            color: sherpablue[600]
-        },
-        integer: {
-            color: seafoam[600]
         }
     })
 )
@@ -31,16 +15,20 @@ const useStyles = makeStyles((theme: Theme) =>
 const ApiExampleModel: React.FunctionComponent<any> = props => {
     const classes = useStyles({})
     const { example } = props;
-    return (
-        <React.Fragment>
-            <Typography variant="h5">Example Request Body</Typography>
-            <pre className={classes.pre}>
-                <code className="language-json">
-                    {JSON.stringify(example, null, 2)}
-                </code>
-            </pre>
-        </React.Fragment>
-    )
+    if (example) {
+        return (
+            <React.Fragment>
+                <Typography variant="h5">Example Request Body</Typography>
+                <pre className={classes.pre}>
+                    <code className="language-json">
+                        {JSON.stringify(example, null, 2)}
+                    </code>
+                </pre>
+            </React.Fragment>
+        )
+    } else {
+        return null;
+    }
 }
 
 export default ApiExampleModel;
