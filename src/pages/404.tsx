79cd0b80c240d854a5bd3602d2ec03d2a3849e91
Eric Sitarz <esitarz@four51.com>
2019-12-16 16:34:37 -0600

    Add Levitation to 404 graphic

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 import Layout from '../components/Layout/Layout'
 import SEO from '../components/seo'
-import svg404 from '../assets/svg/404-graphic.svg'
+import svg404lady from '../assets/svg/lady.svg'
+import svg404cloud from '../assets/svg/cloud.svg'
 import '../components/layout.css'
 import { Link } from 'gatsby'
 
@@ -11,7 +12,10 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
     <div className="container__404">
       <div className="card__404">
-        <img src={svg404} alt="svg img" />
+        <div>
+          <img src={svg404lady} className="levitate" alt="svg img" />
+          <img src={svg404cloud} alt="svg img" />
+        </div>
         <h1>404</h1>
         <h3>Page Not Found</h3>
         <Link to="/">Go Home</Link>
