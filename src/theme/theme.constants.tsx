48f6d2815a14cbc7c2cdb96dc78f53e401dc3cb9
Eric Sitarz <esitarz@four51.com>
2020-01-06 14:13:27 -0600

    OP-82 Optimize Geomtria Font Import

--- src/theme/theme.constants.tsx
+++ src/theme/theme.constants.tsx
@@ -1,7 +1,16 @@
 import { createMuiTheme } from '@material-ui/core/styles'
 import { sherpablue, seafoam, sizzlingred } from './ocPalette.constants'
 import CoreSansWoff from '../assets/fonts/CoreSansD/coresansd-regular.woff'
-import GeometriaWoff from '../assets/fonts/Geometria/geometria-bold.woff'
+import GeometriaWoff400 from '../assets/fonts/Geometria/Geomatria_400/3A0B9A_27_0.woff'
+import GeometriaWoff500 from '../assets/fonts/Geometria/Geomatria_500/3A0B9A_26_0.woff'
+import GeometriaWoff600 from '../assets/fonts/Geometria/Geomatria_600/3A0B9A_21_0.woff'
+import GeometriaWoff700 from '../assets/fonts/Geometria/Geomatria_700/3A0B9A_1C_0.woff'
+import GeometriaWoff800 from '../assets/fonts/Geometria/Geomatria_800/3A0B9A_20_0.woff'
+import GeometriaWoff2400 from '../assets/fonts/Geometria/Geomatria_400/3A0B9A_27_0.woff2'
+import GeometriaWoff2500 from '../assets/fonts/Geometria/Geomatria_500/3A0B9A_26_0.woff2'
+import GeometriaWoff2600 from '../assets/fonts/Geometria/Geomatria_600/3A0B9A_21_0.woff2'
+import GeometriaWoff2700 from '../assets/fonts/Geometria/Geomatria_700/3A0B9A_1C_0.woff2'
+import GeometriaWoff2800 from '../assets/fonts/Geometria/Geomatria_800/3A0B9A_20_0.woff2'
 
 const Geometria = {
   fontFamily: 'Geometria',
@@ -10,7 +19,11 @@ const Geometria = {
   fontWeight: 600,
   src: `
     local('Geometria'),
-    url(${GeometriaWoff}) format('woff')
+    url(${GeometriaWoff400}) format('woff') 400, url(${GeometriaWoff2400}) format('woff2') 400,
+    url(${GeometriaWoff500}) format('woff') 500, url(${GeometriaWoff2500}) format('woff2') 500,
+    url(${GeometriaWoff600}) format('woff') 600, url(${GeometriaWoff2600}) format('woff2') 600,
+    url(${GeometriaWoff700}) format('woff') 700, url(${GeometriaWoff2700}) format('woff2') 700,
+    url(${GeometriaWoff800}) format('woff') 800, url(${GeometriaWoff2800}) format('woff2') 800,
   `,
   unicodeRange:
     'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
