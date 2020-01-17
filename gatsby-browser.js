1c9946dcebccb3cf07ed8cde01bd76b1cbccf117
Robert Watt <rwatt@four51.com>
2020-01-17 16:01:02 -0600

    #146 - Add placeholder for white screen on initial page load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,8 @@
 // https://www.gatsbyjs.org/docs/browser-apis/
-
 require('prismjs/themes/prism-tomorrow.css')
 require('./src/styles/gatsby-highlight.css')
 require('./src/styles/prismjs.css')
+
+exports.onInitialClientRender = () => {
+  document.getElementById('placeholder').classList.add('hide')
+}
