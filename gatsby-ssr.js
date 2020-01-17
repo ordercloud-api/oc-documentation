161e3ee4792e4a0d4cafe4611ff0d100a8bd9be2
Robert Watt <rwatt@four51.com>
2020-01-17 16:11:40 -0600

    #146 - Use src/styles/placeholder.css instead of inline gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,50 +1,17 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-// You can delete this file if you're not using it
 const React = require('react')
+const fs = require('fs')
+const styleContents = fs.readFileSync('./src/styles/placeholder.css')
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-// https://www.gatsbyjs.org/docs/ssr-apis/#onRenderBody
+// https://www.gatsbyjs.org/docs/ssr-apis/#onPreRenderHTML
 exports.onPreRenderHTML = ({
   getPreBodyComponents,
   replacePreBodyComponents,
 }) => {
   // set initial background - avoids blank page while app loads
-  const styleContents = `
-  body > div#placeholder {
-    content: "";
-    position:fixed;
-    left:0;
-    top:0;
-    right:0;
-    bottom:0;
-    background: #fafafa;
-    z-index:10000;
-    opacity:1;
-    transition: opacity 200ms ease-out;
-  }
-  body > div#placeholder.hide {
-    opacity:0;
-    pointer-events:none;
-  }
-  body > div#placeholder:after {
-    content: "";
-    position:fixed;
-    left:0;
-    top:0;
-    right:0;
-    height: 80px;
-    background: #032A43;
-    box-shadow:0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
-    z-index:1;
-  }`
   replacePreBodyComponents([
     <style
       key="placeholder-styles"
