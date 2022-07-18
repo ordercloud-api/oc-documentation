173b220ab3631a8296f350100e9d258daf10d655
Eric Sitarz <ersi@sitecore.net>
2022-07-18 14:17:36 -0500

    OCP-444: Replace Favicon

--- src/components/Templates/ApiReferenceTemplate.tsx
+++ src/components/Templates/ApiReferenceTemplate.tsx
@@ -126,7 +126,7 @@ const ApiReference: FC<ApiReferenceProps> = (props: ApiReferenceProps) => {
         <link
           rel="icon"
           type="image/png"
-          href="/images/favicon.ico"
+          href="https://mss-p-006-delivery.stylelabs.cloud/api/public/content/e496fb48ca514bb2a06a48e7337b68d9"
           sizes="16x16"
         />
       </Helmet>
