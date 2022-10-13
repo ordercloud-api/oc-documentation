f5d0b7485c7f374668b4c8e20c08c5b1ad98972f
Robert Watt <4377246+robertsoniv@users.noreply.github.com>
2022-10-13 12:21:26 -0500

    OCP-494 - tracking tag for 6sense

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -139,6 +139,22 @@ export default (props: LayoutProps) => {
             },
           ]}
         >
+          
+          <script>
+            {`window._6si = window._6si || [];
+            window._6si.push(['enableEventTracking', true]);
+            window._6si.push(['setToken', 'c7a9020fae21b63bab4509b768b4d17c']);
+            window._6si.push(['setEndpoint', 'b.6sc.co']);
+        
+            (function() {
+              var gd = document.createElement('script');
+              gd.type = 'text/javascript';
+              gd.async = true;
+              gd.src = '//j.6sc.co/6si.min.js';
+              var s = document.getElementsByTagName('script')[0];
+              s.parentNode.insertBefore(gd, s);
+            })();`}
+          </script>
           <link
             rel="icon"
             type="image/png"
